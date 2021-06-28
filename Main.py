ed8fadf80d50e0f075c47b4875641deb7d2f774f
H-Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-06-28 21:10:46 +0100

    Close Match system

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 import json
+from difflib import get_close_matches
 data = json.load(open("English.json"))
 
 def defintion(word):
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        return ("Did you mean %s?") % get_close_matches(word, data.keys())[0]
     else: 
         return ("This word was not found!")
 
