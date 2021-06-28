d0fa720c993213a3a2784299242510ca870a94c1
H-Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-06-28 22:03:11 +0100

    Similar word process

--- Main.py
+++ Main.py
@@ -3,10 +3,16 @@ from difflib import get_close_matches
 data = json.load(open("English.json"))
 
 def defintion(word):
+    word = word.lower()
     if word in data:
         return data[word]
     elif len(get_close_matches(word, data.keys())) > 0:
-        return ("Did you mean %s?") % get_close_matches(word, data.keys())[0]
+        choice = input("Did you mean %s?" % get_close_matches(word, data.keys())[0] )
+        choice = choice.lower()
+        if choice == "y":
+            return data[get_close_matches(word, data.keys())[0]]
+        else:
+            return ("This word was not found!")
     else: 
         return ("This word was not found!")
 
