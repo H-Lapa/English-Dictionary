39a46fa4451b54efe22b7c4cf9e68b78204a5a95
H-Lapa <67755960+H-Lapa@users.noreply.github.com>
2021-06-28 16:31:42 +0100

    Base Definition Finder

---
+++ Main.py
@@ -0,0 +1,12 @@
+import json
+data = json.load(open("English.json"))
+
+def defintion(word):
+    if word in data:
+        return data[word]
+    else: 
+        return ("This word was not found!")
+
+
+word = input("select a word:")
+print(defintion(word))
